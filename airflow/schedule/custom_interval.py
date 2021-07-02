c36dd0f186189b0e87ed23c8ef98dc9ae96debfb
Brad Arndt <barndt@trueaccord.com>
2021-07-02 12:22:07 -0500

    add abstract methods to custom interval

--- airflow/schedule/custom_interval.py
+++ airflow/schedule/custom_interval.py
@@ -1,6 +1,7 @@
+import abc
 from datetime import datetime
 
-class CustomInterval():
+class CustomInterval(abc.ABC):
     def __radd__(self, other):
         if not isinstance(other, datetime):
             raise NotImplemented
@@ -18,3 +19,19 @@ class CustomInterval():
     
     def __str__(self):
         return self.__name__
+    
+    @abc.abstractmethod
+    def next(self, dttm):
+        pass
+    
+    @abc.abstractmethod
+    def prev(self, dttm):
+        pass
+    
+    @abc.abstractmethod
+    def __serialize__(self):
+        pass
+    
+    @abc.abstractclassmethod    
+    def __deserialize__(cls, s):
+        pass
